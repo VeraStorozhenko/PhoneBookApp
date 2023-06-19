cbed0d1eb76743cd21ed3ec738bf76b9f84b4a56
vera <verun.mak@gmail.com>
2023-06-19 21:18:35 +0200

    added validation in model

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 Prerequisites
 The setups steps expect following tools installed on the system.
-
 Github
 Ruby 3.1.2
 Rails 7.0.5
@@ -11,7 +10,5 @@ Rails 7.0.5
     bundle exec rake db:setup
 3. Start the Rails server
    You can start the rails server using the command given below.
-
     bundle exec rails s
-
 4. And now you can visit the site with the URL http://localhost:3000
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,5 @@
 class Contact < ApplicationRecord
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :telephone, presence: true
+  validates :first_name, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }, length: { maximum: 50 }
+  validates :last_name, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }, length: { maximum: 50 }
+  validates :telephone, presence: true, numericality: { only_integer: true }, length: {in: 10..15}, uniqueness: true
 end
